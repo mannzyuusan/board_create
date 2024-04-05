d5856708d448d0e9089cc69be7904707c653d5a7
mannzyuusan <gao37441137@gmail.com>
2024-04-05 16:49:43 +0900

    seeder関連

--- database/seeders/SemesterSeeder.php
+++ database/seeders/SemesterSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class SemesterSeeder extends Seeder
 {
@@ -14,6 +16,36 @@ class SemesterSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('semesters')->insert([
+            'id' => '11',//一桁目がSemesterの1,二桁めがこのシート上の1
+            'name' => '1年前期']);
+            
+         DB::table('semesters')->insert([
+            'id' => '12',
+            'name' => '1年後期']);
+            
+         DB::table('semesters')->insert([
+            'id' => '13',
+            'name' => '2年前期']);
+            
+         DB::table('semesters')->insert([
+            'id' => '14',
+            'name' => '2年後期']);
+         
+         DB::table('semesters')->insert([
+            'id' => '15',
+            'name' => '3年前期']);
+            
+         DB::table('semesters')->insert([
+            'id' => '16',
+            'name' => '3年後期']);
+            
+         DB::table('semesters')->insert([
+            'id' => '17',
+            'name' => '4年前期']);
+            
+         DB::table('semesters')->insert([
+            'id' => '18',
+            'name' => '4年後期']);
     }
 }
